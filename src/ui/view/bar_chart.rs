b02fc73f9413c0888c3ce3c05f8b1d22769a4dd3
FirePrincess01 <SarahSteinbrecher01@gmx.at>
2024-11-30 14:29:27 +0100

    format

--- src/ui/view/bar_chart.rs
+++ src/ui/view/bar_chart.rs
@@ -3,12 +3,11 @@
 //! Contains the user interface code of this application
 //! Tries to separate the functionality similar to the the Model View Viewmodel pattern
 
+use super::color_palette;
 use egui::Color32;
 use egui_plot::{self, Bar, BarChart, Legend, Plot};
-use super::color_palette;
 
-pub fn bar_chart(ctx: &egui::Context, ui: &mut egui::Ui)
-{   
+pub fn bar_chart(ctx: &egui::Context, ui: &mut egui::Ui) {
     let color0 = Color32::from_hex(color_palette::COLOR_PINK_0).unwrap();
     let color1 = Color32::from_hex(color_palette::COLOR_PINK_1).unwrap();
     let color2 = Color32::from_hex(color_palette::COLOR_PINK_2).unwrap();
@@ -17,7 +16,6 @@ pub fn bar_chart(ctx: &egui::Context, ui: &mut egui::Ui)
     let color5 = Color32::from_hex(color_palette::COLOR_PINK_5).unwrap();
     let color6 = Color32::from_hex(color_palette::COLOR_PINK_6).unwrap();
     let color7 = Color32::from_hex(color_palette::COLOR_PINK_7).unwrap();
-
 
     let bars1 = vec![
         Bar::new(1.0, 5.0).fill(color0),
@@ -39,7 +37,6 @@ pub fn bar_chart(ctx: &egui::Context, ui: &mut egui::Ui)
     let color6 = Color32::from_hex(color_palette::COLOR_BLUE_6).unwrap();
     let color7 = Color32::from_hex(color_palette::COLOR_BLUE_7).unwrap();
 
-
     let bars2 = vec![
         Bar::new(1.0, 5.0).fill(color0),
         Bar::new(2.0, 6.0).fill(color1),
@@ -51,33 +48,18 @@ pub fn bar_chart(ctx: &egui::Context, ui: &mut egui::Ui)
         Bar::new(8.0, 12.0).fill(color7),
     ];
 
-    // ui.horizontal(|ui| {
-        let chart1 = BarChart::new(bars1);
-        let chart2 = BarChart::new(bars2);
+    let chart1 = BarChart::new(bars1);
+    let chart2 = BarChart::new(bars2);
 
-        Plot::new("demo chart")
-            .allow_zoom(false)
-            .allow_drag(false)
-            .allow_scroll(false)
-            .legend(Legend::default())
-            .height(300.0)
-            .width(400.0)
-            .show(ui, |plot_ui| {
-                plot_ui.add(chart1);
-                plot_ui.add(chart2);
-            });
-
-        // Plot::new("demo chart")
-        // .legend(Legend::default())
-        // .show(ui, |plot_ui| {
-        //     plot_ui.add(chart2);
-        // });
-// });
-
-
-
-
-
-
-
+    Plot::new("demo chart")
+        .allow_zoom(false)
+        .allow_drag(false)
+        .allow_scroll(false)
+        .legend(Legend::default())
+        .height(300.0)
+        .width(400.0)
+        .show(ui, |plot_ui| {
+            plot_ui.add(chart1);
+            plot_ui.add(chart2);
+        });
 }
